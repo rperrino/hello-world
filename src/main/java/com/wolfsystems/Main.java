e86cd909d28ac30ab79a7206c96631b8230d8b45
rperrino <Roberto@LAPTOP-UGD4A5QL>
2017-04-05 17:50:54 -0400

    Print improvements. Added an option to configure whether or not the messages will be printed as errors.

--- src/main/java/com/wolfsystems/Main.java
+++ src/main/java/com/wolfsystems/Main.java
@@ -5,6 +5,10 @@ import com.wolfsystems.utils.Utils;
 public class Main {
 
 	public static void main(String[] args) {
+		Utils.setError(true);
+		Utils.print("Begining program...");
+		
+		Utils.setError(false);
 		String txt = "Hello World";
 		
 		if (args.length > 0)
--- src/main/java/com/wolfsystems/utils/Utils.java
+++ src/main/java/com/wolfsystems/utils/Utils.java
@@ -6,12 +6,25 @@ package com.wolfsystems.utils;
  *
  */
 public class Utils {
-
+	
+	private static boolean error = false;
+	
 	/**
 	 * Prints an object to the console window.
 	 * @param message
 	 */
 	public static void print(Object message) {
-		System.out.println(message);
+		if (error)
+			System.err.println(message);
+		else System.out.println(message);
 	}
+	
+	/**
+	 * Set error parameter for future console prints
+	 * @param err
+	 */
+	public static void setError(boolean err) {
+		error = err;
+	}
+	
 }
